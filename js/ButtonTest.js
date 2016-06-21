e1f0184c80b3dd5fd2fc69cc98ca3d309b71880b
Albert Brand <abrand@xebia.com>
2016-06-21 13:06:27 +0200

    add some more tests

--- js/ButtonTest.js
+++ js/ButtonTest.js
@@ -1,6 +1,7 @@
 /* eslint-env mocha */
 import React, {
   Text,
+  TouchableHighlight,
 } from 'react-native';
 import { shallow } from 'enzyme';
 import Button from './Button';
@@ -14,5 +15,13 @@ describe('Button', () => {
 
   it('renders a text', () => {
     wrapper.find(Text).children().should.have.text(text);
+  });
+
+  it('returns a button', () => {
+    wrapper.find(TouchableHighlight).should.exist;
+  });
+
+  it('passes the onPress function', () => {
+    wrapper.find(TouchableHighlight).props().onPress.should.equal(onPress);
   });
 });
