51f59c58b5f302ebb0c887975eb0d3eee75ef26b
Albert Brand <abrand@xebia.com>
2016-06-21 09:44:25 +0200

    two rows with flex elements and fixed spacing

--- js/App.js
+++ js/App.js
@@ -9,40 +9,49 @@ import {
 export default class reactNativeIntro extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <View style={[styles.box, {flex: 2}]}>
-          <Text style={styles.boxText}>1</Text>
+      <View style={styles.root}>
+        <View style={styles.container}>
+          <View style={[styles.box, {flex: 2}]}>
+            <Text style={styles.boxText}>1</Text>
+          </View>
+          <View style={[styles.box, {flex: 1}]}>
+            <Text style={styles.boxText}>2</Text>
+          </View>
+          <View style={[styles.box, {flex: 9}]}>
+            <Text style={styles.boxText}>3</Text>
+          </View>
         </View>
-        <View style={[styles.box, {flex: 1}]}>
-          <Text style={styles.boxText}>2</Text>
-        </View>
-        <View style={[styles.box, {flex: 9}]}>
-          <Text style={styles.boxText}>3</Text>
-        </View>
-        <View style={[styles.box, {flex: 1}]}>
-          <Text style={styles.boxText}>4</Text>
-        </View>
-        <View style={[styles.box, {flex: 1}]}>
-          <Text style={styles.boxText}>5</Text>
+        <View style={styles.container}>
+          <View style={[styles.box, {flex: 3}]}>
+            <Text style={styles.boxText}>4</Text>
+          </View>
+          <View style={[styles.box, {flex: 4}]}>
+            <Text style={styles.boxText}>5</Text>
+          </View>
         </View>
       </View>
     );
   }
 }
 
+const SPACING = 10;
 const styles = StyleSheet.create({
-  container: {
+  root: {
     flex: 1,
-    flexDirection: 'row',
-    marginTop: 20,
     backgroundColor: '#def',
+    marginTop: 20,
+    paddingVertical: SPACING/2,
+  },
+  container: {
+    flexDirection: 'row',
+    paddingHorizontal: SPACING/2,
   },
   box: {
     height: 50,
     backgroundColor: '#6c1d5f',
     justifyContent: 'center',
     alignItems: 'center',
-    margin: 10,
+    margin: SPACING/2,
   },
   boxText: {
     color: 'white',
